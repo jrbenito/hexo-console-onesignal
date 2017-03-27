986db6b964cef43ac137e70de87cbae600c424d0
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2017-03-27 17:31:55 -0300

    Using request instead of https, this follows redirect
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var https = require('https');
+var log = hexo.log;
+var request = require('request');
 var fs = require('fs');
 var url = 'https://github.com/OneSignal/OneSignal-Website-SDK/releases/download/https-integration-files/OneSignal-Web-SDK-HTTPS-Integration-Files.zip';
 var download = function(url, dest, cb) {
   var file = fs.createWriteStream(dest);
-  var request = https.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close(cb);  // close() is async, call cb after close completes.
-    });
-  }).on('error', function(err) { // Handle errors
-    fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    if (cb) { 
-		cb(err.message);
-	}
-  });
+  request
+    .get(url)
+    .on('response', function(response) {
+        log.log("HTTP request: ",response.statusCode);
+    })
+    .on('error', function(err) {
+        log.log("Download error: ", err);
+        fs.unlink();
+        if (cb) {
+            cb(err.message);
+        }
+    })
+    .pipe(file.on('finish', function() {
+                    file.close(cb);  // close() is async, call cb after close completes.
+                })
+    );
 };
 
 var options = {
@@ -22,8 +28,6 @@ var options = {
     ],
     usage: '<option>'
 };
-
-var log = hexo.log;
 
 hexo.extend.console.register('onesignal', 'Manage Onesignal script/XML files', options, function(args){
     log.log('Downloading: ', 'https://github.com/OneSignal/OneSignal-Website-SDK/releases/download/https-integration-files/OneSignal-Web-SDK-HTTPS-Integration-Files.zip');
