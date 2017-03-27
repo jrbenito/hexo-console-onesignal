eb231f0e44718af6002612d30d1c4dbe5a91eb76
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2017-03-27 16:49:59 -0300

    Downloading file from Onesignal github repo
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var https = require('https');
+var fs = require('fs');
+var url = 'https://github.com/OneSignal/OneSignal-Website-SDK/releases/download/https-integration-files/OneSignal-Web-SDK-HTTPS-Integration-Files.zip';
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) { 
+		cb(err.message);
+	}
+  });
+};
+
 var options = {
     options: [
         {name: '-i, --install', desc: 'Download and install files'}
@@ -8,5 +26,8 @@ var options = {
 var log = hexo.log;
 
 hexo.extend.console.register('onesignal', 'Manage Onesignal script/XML files', options, function(args){
-    log.log('onesignal', 'teste log');
+    log.log('Downloading: ', 'https://github.com/OneSignal/OneSignal-Website-SDK/releases/download/https-integration-files/OneSignal-Web-SDK-HTTPS-Integration-Files.zip');
+
+    download(url,'onesignal.zip');
+    
 });
